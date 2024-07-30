178f9adfabd11e10835deb005fc8e0335f3e313e
Saranyaos <166812821+Saranyaos@users.noreply.github.com>
2024-07-30 14:32:25 +0530

    initial commit

---
+++ src/main/java/com/yaazhtech/Healthcare/management/HealthcareManagementApplication.java
@@ -0,0 +1,13 @@
+package com.yaazhtech.Healthcare.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yaazhtech/Healthcare/management/HealthcareManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaazhtech.Healthcare.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
